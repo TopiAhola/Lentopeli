d9505f087977992f8b7577fa518509d76d6104bc
Veijo Kasanen <veijok119@gmail.com>
2024-10-01 12:28:17 +0300

    Valikko funktio

--- Python tiedostot/valikko.py
+++ Python tiedostot/valikko.py
@@ -1,7 +1,18 @@
 #Valikko funktio
 def valikko():
     print("\n1. Lentokentät \n2. Tavoite lentokentät \n3. Pelin säännöt")
-    valinta = int(input("Paina numeroa"))
+    valinta = int(input("Syötä numero: "))
+
+    # Lentokentät
+    if valinta == 1:
+        print("Lentokentät funktio")
+    
+    #Tavoite lentokentät
+    elif valinta == 2:
+        print("Tavoite lentokentät funktio")
+
+    elif valinta == 3:
+        print("Säännöt funktio")
 
 while True:
     valikko()

ff91eb56d89dbd83f9c1ff899450ec7c6af5a08b
Mikaemik <mikael.e.mikkola@gmail.com>
2024-10-09 13:56:39 +0300

    Lisätty pelinsäännöt

--- Python tiedostot/Turisti.py
+++ Python tiedostot/Turisti.py
@@ -1,3 +1,30 @@
+def alkufunktio():
+    print("--- Tervetuloa Urheilijan Jalkapalloseikkailu Euroopassa! ---")
+    print(
+        "\nTässä pelissä sinä olet jalkapalloilija, joka osallistuu kansainvälisiin turnauksiin viidessä eri Euroopan maassa.")
+    print("Tavoitteesi on päästä kaikkiin viiteen maahan, joissa pelataan jalkapallo-otteluita.")
+    print("Aloitat seikkailusi yhdestä Euroopan maasta ja saat 1500 € sponsorirahaa matkallesi.")
+    print(
+        "Valitse yksi kolmesta lähimmistä lentokentästä ja tee valintoja, jotka vaikuttavat päästöihisi ja kuluihisi.")
+    print("Kun saavut uuteen maahan saat palkkion kunniaosoituksena.")
+    print("Kunniaosoituksen määrä riippuu kunkin valtion varallisuudesta (maan BKT).")
+
+    print("\n--- Lentojen hinnat ja päästöt ---")
+    print("1. Vähänpäästöinen luokka:")
+    print("   - Päästöt: 100 g CO2 per kilometri")
+    print("   - Hinta: 0,3 € per kilometri")
+    print("2. Keskipäästöinen luokka:")
+    print("   - Päästöt: 200 g CO2 per kilometri")
+    print("   - Hinta: 0,2 € per kilometri")
+    print("3. Suurpäästöinen luokka:")
+    print("   - Päästöt: 300 g CO2 per kilometri")
+    print("   - Hinta: 0,1 € per kilometri\n")
+
+    print("Pelin tavoitteena on käydä kaikissa viidessä maassa mahdollisimman pienillä päästöillä.")
+    print("Mikäli rahasi loppuvat, peli päättyy.")
+    print(
+        "Voit voittaa pelin ja päästä kunnioitustauluun, jos onnistut saavuttamaan kaikki viisi kohdetta vähillä päästöillä!")
+
 def end_game(game_id):
 #Pelaaja voittaa pelin. Tulostetaan lopputiedot.
     game_id, game_name, game_location, game_money, game_co2, game_money_gained, game_money_spent, game_distance, game_flights = game_values(game_id)
@@ -497,6 +524,7 @@ kursori = yhteys.cursor()
 
 # Luodaan uusi peli tai ladataan vanha: tämä määrittää game_id:n
 game_type = input("Valitse 1 tai 2:\n1 Uusi peli\n2 Lataa peli\n")
+alkufunktio()
 if game_type == "2":
     game_id = load_game()
 
